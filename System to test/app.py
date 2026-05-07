eea799a10330d2a6097eaacd2218ed00b65a8f42
prabeshnpl <nepalprabesh99@gmail.com>
2026-05-07 17:57:31 +0545

    third draft. Also edited system to test for displaying probabilities of prediction.

--- System to test/app.py
+++ System to test/app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request
 import joblib
 from utils.modules.preprocessing_v1 import clean_and_lemmatize
 
-NB_model = joblib.load('utils/models/v5/sentiment_detection_nb_model_v5.pkl')
-SVM_model = joblib.load('utils/models/v5/sentiment_detection_svm_model_v5.pkl')
-tf_idf = joblib.load('utils/models/v5/tfidf_vectorizer_v5.pkl')
+NB_model = joblib.load('utils/models/v7/sentiment_detection_nb_model_v7.pkl')
+SVM_model = joblib.load('utils/models/v7/sentiment_detection_svm_model_v7.pkl')
+tf_idf = joblib.load('utils/models/v7/tfidf_vectorizer_v7.pkl')
 
 app = Flask(__name__)
 
@@ -14,6 +14,8 @@ def index():
     nb_prediction = None
     svm_prediction = None
     input_text = ''
+    nb_prediction_probs = ''
+    svm_prediction_scores = ''
 
     if request.method == 'POST':
 
@@ -24,13 +26,17 @@ def index():
         processed_text = clean_and_lemmatize(input_text)
 
         # Transform into digits
-        X = tf_idf.transform([processed_text])
-        
+        X = tf_idf.transform(processed_text)
+
         # Predict using Naive Bayes' model
         nb_prediction = NB_model.predict(X)
+        nb_prediction_probs = NB_model.predict_proba(X)[0]
+        print(f"Negative :{nb_prediction_probs[0]}\nNeutral : {nb_prediction_probs[1]}\nPositive: {nb_prediction_probs[2]}")
 
         # Predict using SVM model
         svm_prediction = SVM_model.predict(X)
+        svm_prediction_scores = SVM_model.decision_function(X)[0]
+        print(f"Negative :{svm_prediction_scores[0]}\nNeutral : {svm_prediction_scores[1]}\nPositive: {svm_prediction_scores[2]}")
 
         # Convert output
         label_map = {-1: "Negative", 0: "Neutral", 1: "Positive"}
@@ -41,7 +47,9 @@ def index():
         'index.html', 
         input_text=input_text,
         nb_prediction=nb_prediction,
-        svm_prediction=svm_prediction
+        nb_probability=nb_prediction_probs,
+        svm_prediction=svm_prediction,
+        svm_prediction_scores=svm_prediction_scores
     )
 
 if __name__ == '__main__':
